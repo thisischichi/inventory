c647659198c8981650fd560efd18463cd4cc7712
Chi-chi <thisischichi@gmail.com>
2019-06-03 21:08:16 +0100

    retrieve and display products

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    products: []
+  }
+
+  componentDidMount() {
+    this.getProducts();
+  }
+  
+  getProducts = _ => {
+    fetch('http://localhost:3010/products')
+    .then(response => response.json())
+    .then(response => this.setState({ products: response.data })) 
+    .catch(err => console.error(err))
+  }
+
+  renderProduct = ({ id, name }) => <div key={id}>{name}</div>
+
+  renderTable = ({ name, current_count, manual_count }) => 
+    <tr>
+      <td>{name}</td>
+      <td>{current_count}</td>
+      <td>{manual_count}</td>
+    </tr>
+
+  render() {
+    const { products } = this.state;
+    return (
+      <div className="App">
+        <h1>Simple Inventory</h1>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Current Count</th>
+            <th>Manual Count</th>
+          </tr>
+          {products.map(this.renderTable)}
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
